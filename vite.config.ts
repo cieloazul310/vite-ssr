c6f65aafc6bb5635f1f70415f7b59667dcb98367
cieloazul310 <cieloazul310@gmail.com>
2023-02-13 21:42:33 +0900

    ci: add deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import ssr from 'vite-plugin-ssr/plugin';
 import mdx from '@mdx-js/rollup';
 
 const config = defineConfig({
+  base: '/vite-ssr',
   plugins: [react(), mdx({ jsxImportSource: '@emotion/react' }), ssr()],
 });
 
